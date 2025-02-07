cfd39c246a7e7aa66570194c50eb854e7f79fb22
Mahesh Sunkula <maheshbabu.sunkula@gmail.com>
2025-02-07 12:52:44 +0530

    Model Algorithams

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 
 def save_object(file_path, obj):
     try:
@@ -20,13 +22,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
-            model = list(models.values())[i]
+            model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             # model.fit(X_train,y_train) # Train Model
@@ -41,6 +48,5 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
 
         return report
-    
     except Exception as e:
-        raise CustomException (e, sys)
+        raise CustomException(e, sys)
